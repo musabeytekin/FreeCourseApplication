b52ecfb7e10e7e73caa56574cd25fe09f930f774
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-09-13 15:17:17 +0300

    Courses controller: update Course

--- Web/WebMVC/FreeCourse.Web/Controllers/CoursesController.cs
+++ Web/WebMVC/FreeCourse.Web/Controllers/CoursesController.cs
@@ -43,13 +43,58 @@ public class CoursesController : Controller
         if (!ModelState.IsValid)
         {
             return View();
-
         }
-
 
         input.UserId = _sharedIdentityService.GetUserId;
         var result = await _catalogService.CreateCourseAsync(input);
-        
+
         return RedirectToAction(nameof(Index));
     }
+    
+
+    public async Task<IActionResult> Update(string id)
+    {
+        var course = await _catalogService.GetByCourseId(id);
+        var categories = await _catalogService.GetAllCategoryAsync();
+
+        if (course is null)
+        {
+            RedirectToAction(nameof(Index));
+        }
+        
+        ViewBag.Categories = new SelectList(categories, "Id", "Name", course!.Id);
+        
+        CourseUpdateInput courseUpdateInput = new()
+        {
+            Id = course.Id,
+            Name = course.Name,
+            Description = course.Description,
+            Price = course.Price,
+            Feature = course.Feature,
+            CategoryId = course.CategoryId,
+            UserId = course.UserId,
+            Picture = course.Picture
+        };
+
+        return View(courseUpdateInput);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Update(CourseUpdateInput input)
+    {
+        var categories = await _catalogService.GetAllCategoryAsync();
+
+        ViewBag.Categories = new SelectList(categories, "Id", "Name", input.Id);
+
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+
+        var result = await _catalogService.UpdateCourseAsync(input);
+
+        return RedirectToAction(nameof(Index));
+    }
+    
+    
 }

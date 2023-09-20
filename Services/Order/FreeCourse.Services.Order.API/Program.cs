46c213d0bde2bdcbc532c57550f6ce096b470e1c
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-09-20 08:55:10 +0300

    fix: Apply migrations to OrderDb at init

--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -59,7 +59,11 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+    db.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
